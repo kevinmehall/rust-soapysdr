f976390b2aaeb06d8cc65a4dbf9229b0c5b7b353
James Cook <mokus@deepbondi.net>
2020-02-28 20:53:29 -0800

    use pkg-config -I flags to find SoapySDR headers

--- soapysdr-sys/build.rs
+++ soapysdr-sys/build.rs
@@ -5,14 +5,21 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    if let Err(e) = pkg_config::Config::new().atleast_version("0.6.0").probe("SoapySDR") {
-        panic!("Couldn't find SoapySDR: {}", e);
-    }
+    let lib = match pkg_config::Config::new().atleast_version("0.6.0").probe("SoapySDR") {
+        Err(e) => panic!("Couldn't find SoapySDR: {}", e),
+        Ok(lib) => lib,
+    };
 
-    let bindings = bindgen::Builder::default()
+    let mut bindings = bindgen::Builder::default()
         .trust_clang_mangling(false)
-        .header("wrapper.h")
-        .generate()
+        .header("wrapper.h");
+    
+    for inc in lib.include_paths {
+        let inc_str = inc.to_str().expect("PathBuf to string conversion problem");
+        bindings = bindings.clang_arg("-I".to_owned() + inc_str);
+    }
+    
+    let bindings = bindings.generate()
         .expect("Unable to generate bindings");
 
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
