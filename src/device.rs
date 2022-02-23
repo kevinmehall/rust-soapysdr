c220b043ebdcba938512def36d093f64cc9b5883
wcampbell <wcampbell1995@gmail.com>
2022-02-22 23:08:06 -0500

    fix: address MR suggestions

--- src/device.rs
+++ src/device.rs
@@ -835,12 +835,10 @@ impl Device {
 
     // TODO: registers
 
-    // TODO: settings
-
     /// Write a setting
     pub fn write_setting<S: Into<Vec<u8>>>(&self, key: S, value: S) -> Result<(), Error> {
-        let key = CString::new(key).expect("key must contain null byte");
-        let value = CString::new(value).expect("value must contain null byte");
+        let key = CString::new(key).expect("key must not contain null byte");
+        let value = CString::new(value).expect("value must not contain null byte");
         unsafe {
             check_ret_error(SoapySDRDevice_writeSetting(self.inner.ptr, key.as_ptr(), value.as_ptr()))?;
             Ok(())
@@ -849,7 +847,7 @@ impl Device {
 
     /// Read a setting
     pub fn read_setting<S: Into<Vec<u8>>>(&self, key: S) -> Result<String, Error> {
-        let key = CString::new(key).expect("key must contain null byte");
+        let key = CString::new(key).expect("key must not contain null byte");
         unsafe {
             string_result(SoapySDRDevice_readSetting(self.inner.ptr, key.as_ptr()))
         }
