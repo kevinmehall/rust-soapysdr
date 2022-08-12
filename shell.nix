a536b7f11c1ae9ab94a3bb0e1e48af6616063c07
GU Junhua <jhgu@nao.cas.cn>
2022-08-12 21:09:00 +0800

    modified:   shell.nix

--- shell.nix
+++ shell.nix
@@ -8,4 +8,11 @@ stdenv.mkDerivation rec {
     llvm
   ];
   LIBCLANG_PATH="${llvmPackages.libclang.lib}/lib";
+  BINDGEN_EXTRA_CLANG_ARGS="$(< ${stdenv.cc}/nix-support/libc-crt1-cflags) \
+      $(< ${stdenv.cc}/nix-support/libc-cflags) \
+      $(< ${stdenv.cc}/nix-support/cc-cflags) \
+      $(< ${stdenv.cc}/nix-support/libcxx-cxxflags) \
+      ${lib.optionalString stdenv.cc.isClang "-idirafter ${stdenv.cc.cc}/lib/clang/${lib.getVersion stdenv.cc.cc}/include"} \
+      ${lib.optionalString stdenv.cc.isGNU "-isystem ${stdenv.cc.cc}/include/c++/${lib.getVersion stdenv.cc.cc} -isystem ${stdenv.cc.cc}/include/c++/${lib.getVersion stdenv.cc.cc}/${stdenv.hostPlatform.config} -idirafter ${stdenv.cc.cc}/lib/gcc/${stdenv.hostPlatform.config}/${lib.getVersion stdenv.cc.cc}/include"} \
+    ";
 }
