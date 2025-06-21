ec31ac41f0c24a81128482f08aa0b33f6ab01a08
Kevin Mehall <km@kevinmehall.net>
2025-06-21 11:59:27 -0600

    shell.nix: use bindgenHook

--- shell.nix
+++ shell.nix
@@ -7,15 +7,6 @@ stdenv.mkDerivation rec {
     pkg-config
     llvm
     cargo
+    rustPlatform.bindgenHook
   ];
-
-  # https://hoverbear.org/blog/rust-bindgen-in-nix/
-  LIBCLANG_PATH="${llvmPackages.libclang.lib}/lib";
-  BINDGEN_EXTRA_CLANG_ARGS="$(< ${stdenv.cc}/nix-support/libc-crt1-cflags) \
-      $(< ${stdenv.cc}/nix-support/libc-cflags) \
-      $(< ${stdenv.cc}/nix-support/cc-cflags) \
-      $(< ${stdenv.cc}/nix-support/libcxx-cxxflags) \
-      ${lib.optionalString stdenv.cc.isClang "-idirafter ${stdenv.cc.cc}/lib/clang/${lib.getVersion stdenv.cc.cc}/include"} \
-      ${lib.optionalString stdenv.cc.isGNU "-isystem ${stdenv.cc.cc}/include/c++/${lib.getVersion stdenv.cc.cc} -isystem ${stdenv.cc.cc}/include/c++/${lib.getVersion stdenv.cc.cc}/${stdenv.hostPlatform.config} -idirafter ${stdenv.cc.cc}/lib/gcc/${stdenv.hostPlatform.config}/${lib.getVersion stdenv.cc.cc}/include"} \
-    ";
 }
